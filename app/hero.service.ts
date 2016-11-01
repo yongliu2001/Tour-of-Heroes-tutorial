bf096277d8ee5b82bc4ed4045e91c83a9ac0f1d5
Yong Liu <yong.liu2001@gmail.com>
2016-11-01 23:07:26 +1100

    add router function

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,12 +4,21 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
+
     getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise<Hero[]> (resolve => 
+        return new Promise<Hero[]> (resolve =>
             setTimeout(resolve, 2000)) //delay 2 seconds
             .then(() => this.getHeroes());
     }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+          .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    
 }
